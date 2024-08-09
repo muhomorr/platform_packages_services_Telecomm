ccf524181bcecfe0279a852b75f27dc379653525
Grant Menke <grantmenke@google.com>
2024-08-09 17:07:01 +0000

    Revert  "Unbind CS if connection is not created within 15 seconds."
    This reverts commit 286781dfcb78d8b5c1a77f2390f5251f01943add.
    
    Reason for revert: Causes NPE (b/293458004); will submit a clean fix after revert
    
    Merged-In: Ib845c6d52c5839b12cb9f8fa8b72cd0c7b10dbe5
    
    Merged-In: I32fe284a867f1fbd3aa3064023723186a2ba529f
    
    Merged-In: I0ee92a22dd6b3d505899c896b9cb5ec2fbd521f6
    
    Change-Id: Ie6376f75acf0fb0d1050819b3e494e3bf9248dd0

--- tests/src/com/android/server/telecom/tests/BasicCallTests.java
+++ tests/src/com/android/server/telecom/tests/BasicCallTests.java
@@ -978,7 +978,6 @@ public class BasicCallTests extends TelecomSystemTest {
         call.setTargetPhoneAccount(mPhoneAccountA1.getAccountHandle());
         assert(call.isVideoCallingSupportedByPhoneAccount());
         assertEquals(VideoProfile.STATE_BIDIRECTIONAL, call.getVideoState());
-        call.setIsCreateConnectionComplete(true);
     }
 
     /**
@@ -1002,7 +1001,6 @@ public class BasicCallTests extends TelecomSystemTest {
         call.setTargetPhoneAccount(mPhoneAccountA2.getAccountHandle());
         assert(!call.isVideoCallingSupportedByPhoneAccount());
         assertEquals(VideoProfile.STATE_AUDIO_ONLY, call.getVideoState());
-        call.setIsCreateConnectionComplete(true);
     }
 
     /**
