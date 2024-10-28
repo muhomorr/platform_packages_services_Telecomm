9934a1572e683b29e0f65e2d081b0b3edb89167e
Grant Menke <grantmenke@google.com>
2024-10-28 16:42:25 -0700

    Unbind CS if connection is not created within 15 seconds.
    This CL adds a check to ensure that connection creation occurs within 15 seconds after binding to that ConnectionService. If the connection/conference is not created in that timespan, this CL adds logic to manually unbind the ConnectionService at that point in time. This prevents malicious apps from keeping a declared permission in forever even in the background. This updated CL includes a null check to avoid a NPE that occurred in the first iteration of this change.
    
    Bug: 293458004
    Test: manually using the provided apk + atest CallsManagerTest
    Flag: EXEMPT Security High/Critical Severity CVE
    (cherry picked from commit 48d6b0df91fb1c8c0b11891f878084c6d8f9fa8a)
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:ed874348d2e9f4a219381ac646452783fc91fbca)
    Merged-In: If46cfa26278f09854c10266af6eaa73382f20296
    Change-Id: If46cfa26278f09854c10266af6eaa73382f20296

--- tests/src/com/android/server/telecom/tests/CallsManagerTest.java
+++ tests/src/com/android/server/telecom/tests/CallsManagerTest.java
@@ -44,6 +44,7 @@ import static org.mockito.Mockito.timeout;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static java.lang.Thread.sleep;
 
 import static java.lang.Thread.sleep;
 
@@ -57,6 +58,7 @@ import android.media.AudioManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.IBinder;
 import android.os.Looper;
 import android.os.OutcomeReceiver;
 import android.os.Process;
@@ -104,6 +106,7 @@ import com.android.server.telecom.ClockProxy;
 import com.android.server.telecom.ConnectionServiceFocusManager;
 import com.android.server.telecom.ConnectionServiceFocusManager.ConnectionServiceFocusManagerFactory;
 import com.android.server.telecom.ConnectionServiceWrapper;
+import com.android.server.telecom.CreateConnectionResponse;
 import com.android.server.telecom.DefaultDialerCache;
 import com.android.server.telecom.EmergencyCallDiagnosticLogger;
 import com.android.server.telecom.EmergencyCallHelper;
@@ -3772,5 +3775,20 @@ public class CallsManagerTest extends TelecomTestCase {
         TelephonyManager mockTelephonyManager = mComponentContextFixture.getTelephonyManager();
         when(mockTelephonyManager.getPhoneCapability()).thenReturn(mPhoneCapability);
         when(mPhoneCapability.getMaxActiveVoiceSubscriptions()).thenReturn(num);
+    }
+
+    private void waitUntilConditionIsTrueOrTimeout(Condition condition, long timeout,
+            String description) throws InterruptedException {
+        final long start = System.currentTimeMillis();
+        while (!condition.expected().equals(condition.actual())
+                && System.currentTimeMillis() - start < timeout) {
+            sleep(50);
+        }
+        assertEquals(description, condition.expected(), condition.actual());
+    }
+
+    protected interface Condition {
+        Object expected();
+        Object actual();
     }
 }
